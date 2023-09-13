24cc79eefc6e69981983236abb33d148b566039d
Wen-Wei Tseng <sosiristseng@gmail.com>
2023-09-13 14:22:50 +0800

    disable RSS

--- quartz.config.ts
+++ quartz.config.ts
@@ -63,7 +63,7 @@ const config: QuartzConfig = {
       Plugin.TagPage(),
       Plugin.ContentIndex({
         enableSiteMap: true,
-        enableRSS: true,
+        enableRSS: false,
       }),
       Plugin.Assets(),
       Plugin.Static(),
