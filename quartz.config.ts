11033a1da871b8e044c52b6b3dd7e1bfa717d82b
Wen Wei Tseng <sosiristseng@gmail.com>
2025-11-16 15:50:00 +0800

    mathjax

--- quartz.config.ts
+++ quartz.config.ts
@@ -71,7 +71,7 @@ const config: QuartzConfig = {
       Plugin.TableOfContents(),
       Plugin.CrawlLinks({ markdownLinkResolution: "shortest" }),
       Plugin.Description(),
-      Plugin.Latex({ renderEngine: "katex" }),
+      Plugin.Latex({ renderEngine: "mathjax" }),
     ],
     filters: [Plugin.RemoveDrafts()],
     emitters: [
